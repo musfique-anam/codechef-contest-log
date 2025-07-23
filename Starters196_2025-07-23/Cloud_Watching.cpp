da438d8fb8c21e3a89621c1da2de9e76e7b94c16
Musfique Anam <musfique2002.personal@gmail.com>
2025-07-23 21:42:05 +0600

    updated

---
+++ Starters196_2025-07-23/Cloud_Watching.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int A,B;
+	cin>>A>>B;
+	int c=A*3;
+	if(c<=B){
+	    cout<<"Rain"<<endl;
+	}else{
+	    cout<<"Dry"<<endl;
+	}
+
+}
---
+++ Starters196_2025-07-23/More_Cookies.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int problem(int Eq,bool L,int C){
+
+    if(L = 0 && Eq>0){
+        C=C+1;
+    }
+
+    problem(Eq,L,C);
+}
+
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int N,C,E=0,L=0;
+        cin>>N>>C;
+
+        vector<int> v(N);
+
+        for(int i=0;i<N;i++){
+            cin>>v[i];
+        }
+
+        bool less=false;
+        int equal=0;
+
+        for(int i=0;i<N;i++){
+            if(v[i] < C){
+                less=true;
+                break;
+            }
+        }
+
+        for(int i=0;i<N;i++){
+            if(v[i] == C){
+                equal=i;
+                E=v[equal];
+                break;
+            }
+        }
+
+
+        problem(E,less,C);
+
+
+
+
+        
+
+        
+    }
+
+}
